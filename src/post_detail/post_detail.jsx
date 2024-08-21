bb0313fffa06032e0a1fcba0a3b49061ac1c810e
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-08-21 22:18:43 +0900

    fix postUser

--- src/post_detail/post_detail.jsx
+++ src/post_detail/post_detail.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Box, Text } from "@yamada-ui/react";
+import { Box, Text, Image } from "@yamada-ui/react";
 import './css/post_detail.css';
 
 const PostDetail = () => {
     const { id } = useParams();
     const [postData, setPostData] = useState(null);
+    const [postUser, setPostUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
@@ -30,6 +31,29 @@ const PostDetail = () => {
         fetchPostDetail();
     }, [id]);
 
+    useEffect(() => {
+        if (postData && postData.id) {
+        const fetchPostUser = async () => {
+            try {
+                const response = await fetch(`http://127.0.0.1:5000/users/${postData.id}`,{
+                    method: 'GET',
+                });
+                if (response.ok) {
+                    const data = await response.json();
+                    setPostUser(data);
+                    setLoading(false);
+                } else {
+                    console.error('Failed to data');
+                    setLoading(false);
+                }
+            } catch (error) {
+                console.error('Error to falid post Data:',error);
+                setLoading(false);
+            }
+        };
+        fetchPostUser();}
+    }, [postData]);
+
     if (loading) {
         return <p>loading...</p>;
     }
@@ -40,9 +64,22 @@ const PostDetail = () => {
 
     return (
         <>
-            <Box mt={100}>
+            <Box className="postdetail">
                 <h1>モヤモヤ詳細</h1>
                 <Text>{postData.post}</Text>
+            {postUser ? (
+                <Box>
+                    <Text>{postUser.name}</Text>
+                    {postUser.prof_image ? (
+                        <Image src={`http://127.0.0.1:5000/prof_image/${postUser.prof_image}`} alt="prof_image" />
+
+                    ) : (
+                        <Image src="/not_profileicon.jpg" />
+                    )}
+                </Box>
+            ) : (
+                <p>Loading user data...</p>
+            )}
             </Box>
         </>
     );
