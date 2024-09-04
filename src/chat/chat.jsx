f6220189e72b7f1675948849ba3980aaed363a05
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-09-04 22:40:04 +0900

    add user_select.jsx,css

---
+++ src/chat/chat.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+import io from 'socket.io-client';
+
+
+const Chat = ({receiver_id}) => {
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+    const [userData, setUserData] = useState(null);
+
+    const socket = io.connect('http://localhost:5000');
+
+    useEffect(() => {
+        socket.on('receive_message', (data) => {
+          setMessages((prevMessages) => [...prevMessages, data]);
+        });
+      }, []);
+    
+      const sendMessage = () => {
+        socket.emit('send_message', {
+          message: message,
+          send_user_id: ,
+          receiver_user_id: 2  // 受信者IDも仮（動的に変更可能）
+        });
+        setMessage("");
+      };
+}
