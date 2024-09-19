94bf888e365277acb99792f4c09a3af45c585bd2
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-09-19 15:58:51 +0900

    fix follow_button

--- src/user_prof/follow_button/follow_button.jsx
+++ src/user_prof/follow_button/follow_button.jsx
@@ -1,8 +1,32 @@
-import { useState } from "react";
-import '../css/follow_button.css'
+import { useEffect, useState } from "react";
+import '../css/follow_button.css';
+import { Button } from "@yamada-ui/react";
 const Followbutton = ({ userId }) => {
   const [isFollower, setFollower] = useState(false);
 
+
+  useEffect(() => {
+    const checkFollow = async () => {
+      const token = localStorage.getItem('token');
+      try {
+        const response = await fetch(`http://127.0.0.1:5000/follow_type/${userId}`,{
+          method: "GET",
+          headers: {
+            "Authorization": `Bearer ${token}`,
+          },
+        });
+        if (response.ok) {
+          const data = await response.json();
+          setFollower(data.isFollower);
+        } else {
+          console.error('Failed to check follower status');
+        }
+      } catch (error) {
+        console.error('error', error);
+      }
+    };
+    checkFollow();
+  },[userId]);
   const handleFollow = async () => {
     const token = localStorage.getItem("token");
     try {
@@ -26,9 +50,10 @@ const Followbutton = ({ userId }) => {
   };
 
   return (
-      <button onClick={handleFollow} disabled={isFollower} className="follow_button">
+
+      <Button onClick={handleFollow} disabled={isFollower} className="follow_button">
         {isFollower ? "フォロー中" : "フォローする"}
-      </button>
+      </Button>
   );
 };
 
