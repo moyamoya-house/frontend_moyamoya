d09bd6939268184d85d8d33f149b9c94394c5ea4
yamatatsu1215 <tatsuyama3140@gmail.com>
2024-10-12 09:38:12 +0900

    fix notification.jsx

--- src/notification/notification.jsx
+++ src/notification/notification.jsx
@@ -1,9 +1,30 @@
-import { Box } from "@yamada-ui/react"
+import { Box } from "@yamada-ui/react";
+import { useEffect, useState } from "react";
 
 
 const Notification = () => {
+    const [notification, setNotification] = useState([]);
+
+    useEffect(() => {
+        const fetchNotificationData = async () => {
+            const token = localStorage.getItem('token');
+            const response = await fetch('http://127.0.0.1:5000/notification',{
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`,
+                }
+            });
+            if (response.ok) {
+                const data = await response.json();
+                setNotification(data);
+            }
+        };
+        fetchNotificationData();
+    },[]);
     return (
-        <Box>ueueuue</Box>
+        <Box>
+            {notification}
+        </Box>
     )
 }
 
