13f7dc1e3961abfdab3168ed620aeb43d653b82a
yamatatsu1215 <tatsuyama3140@gmail.com>
2025-02-03 22:59:18 +0900

    ログインページのスタイルを改善し、フッターの著作権表記を更新

--- src/login/login.tsx
+++ src/login/login.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Box, Text, Input, Button, Link } from "@yamada-ui/react";
 import { useNavigate } from "react-router-dom";
+import "./css/login.css";// CSSモジュールをインポート
 
 const Login = () => {
   const [username, setUsername] = useState("");
@@ -26,113 +27,55 @@ const Login = () => {
   };
 
   return (
-    <>
-      <Box
-        w={500}
-        h="auto"
-        m="0px auto 20px auto"
-        border="1px solid #000"
-        mt={120}
-        boxShadow="10px 10px 5px gray"
-      >
-        <form onSubmit={hadlelogin}>
-          {/* title */}
-          <Text textAlign="center" fontSize="1.5rem" fontWeight="bold">
-            login
-          </Text>
-          {/* username */}
-          <Box m="40px">
-            <Input
-              type="text"
-              placeholder="Usernameを入力"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              w="90%"
-              padding={10}
-              borderRadius={5}
-              maxLength={20}
-              _focus={{color: "sky.100"}}
-            />
-          </Box>
-          {/* パスワード */}
-          <Box m={40}>
-            <Input
-              type="password"
-              placeholder="Passwordを入力"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              w="90%"
-              padding={10}
-              borderRadius={5}
-            />
-          </Box>
-          {/* loginbutton */}
-          <Box m={40}>
-            <Button
-              type="submit"
-              w="80%"
-              h={50}
-              m="20px auto"
-              colorScheme="secondary"
-              border="none"
-              bg="lightskyblue"
-              borderRadius={10}
-              display={"block"}
-              textAlign={"center"}
-              textColor={"white"}
-              cursor={"pointer"}
-              _hover={{backgroundColor: "purple"}}
-            >
-              ログイン
-            </Button>
-          </Box>
-          {/* signupbutton */}
-          <Box m={40}>
-            <Link
-              href="/signup"
-              textDecoration={"none"}
-              textColor={"white"}
-              w="80%"
-              h={50}
-              bg="lightskyblue"
-              borderRadius={10}
-              m="20px auto"
-              textAlign="center"
-              colorScheme="secondary"
-              display="flex"
-              alignItems={"center"}
-              justifyContent={"center"}
-              cursor={"pointer"}
-              _hover={{backgroundColor: "purple"}}
-            >
-              新規登録へ
-            </Link>
-          </Box>
+    <Box className="container">
+      <form onSubmit={hadlelogin}>
+        {/* title */}
+        <Text className="logintitle">login</Text>
 
-          <Box m={40}>
-            <Link
-              href="/password_reset"
-              textDecoration={"none"}
-              textColor={"white"}
-              w="80%"
-              h={50}
-              bg="lightskyblue"
-              borderRadius={10}
-              m="20px auto"
-              textAlign="center"
-              colorScheme="secondary"
-              display="flex"
-              alignItems={"center"}
-              justifyContent={"center"}
-              cursor={"pointer"}
-              _hover={{backgroundColor: "purple"}}
-            >
-              パスワードを忘れた方へ
-            </Link>
-          </Box>
-        </form>
-      </Box>
-    </>
+        {/* username */}
+        <Box className="inputBox">
+          <Input
+            type="text"
+            placeholder="Usernameを入力"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            className="input"
+          />
+        </Box>
+
+        {/* password */}
+        <Box className="inputBox">
+          <Input
+            type="password"
+            placeholder="Passwordを入力"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            className="input"
+          />
+        </Box>
+
+        {/* login button */}
+        <Box className="buttonBox">
+          <Button type="submit" className="loginbutton">
+            ログイン
+          </Button>
+        </Box>
+
+        {/* signup button */}
+        <Box className="buttonBox">
+          <Link href="/signup" className="buttonlink">
+            新規登録へ
+          </Link>
+        </Box>
+
+        {/* password reset */}
+        <Box className="buttonBox">
+          <Link href="/password_reset" className="buttonlink">
+            パスワードを忘れた方へ
+          </Link>
+        </Box>
+      </form>
+    </Box>
   );
 };
 
